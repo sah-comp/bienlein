778c411f6dbe6921776c7ccc5b8dfb99548ed706
Stephan <info@sah-company.com>
2013-06-24 19:22:25 +0200

    Fixes #4 and add Model_Page::render()

--- src/Model/Page.php
+++ src/Model/Page.php
@@ -18,6 +18,24 @@
 class Model_Page extends Model
 {
     /**
+     * Returns a string with all slices rendered.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $ret = array();
+        foreach ($this->bean->template->ownRegion as $region_id => $region) {
+            $s = '';
+            foreach ($this->getSlicesByRegion($region_id, false) as $slice_id => $slice) {
+                $s .= $slice->render('frontend')."\n";
+            }
+            $ret[$region->name] = $s;
+        }
+        return implode("\n", $ret);
+    }
+
+    /**
      * Returns an array with attributes for lists.
      *
      * @param string (optional) $layout
@@ -118,7 +136,7 @@ SQL;
      */
     public function dispense()
     {
-        $this->bean->language = Flight::get('user')->getLanguage();
+        if (Flight::has('user')) $this->bean->language = Flight::get('user')->getLanguage();
         $this->autoInfo(true);
         $this->addValidator('name', new Validator_HasValue());
         $this->addValidator('language', new Validator_HasValue());
