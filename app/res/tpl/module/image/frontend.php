e2f2559f87e1827ab77f6ea9ee55a6e8a3931660
Stephan <info@sah-company.com>
2013-07-03 11:08:08 +0200

    Image module uses original image size

--- app/res/tpl/module/image/frontend.php
+++ app/res/tpl/module/image/frontend.php
@@ -8,4 +8,9 @@
  * @version $Id$
  */
 ?>
-<img src="<?php echo Flight::get('media_path').'/'.$record->content ?>" alt="" />
+<?php list($width, $height, $type, $attr) = getimagesize(Flight::get('upload_dir').'/'.$record->content); ?>
+<img
+    src="<?php echo Flight::get('media_path').'/'.$record->content ?>"
+    alt=""
+    width="<?php echo $width ?>"
+    height="<?php echo $height ?>" />
