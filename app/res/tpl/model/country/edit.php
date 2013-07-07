f0deb2afe502d313f248eb14d04a1ce859b182d1
Stephan <info@sah-company.com>
2013-07-07 11:18:06 +0200

    Added translations tab for country form

--- app/res/tpl/model/country/edit.php
+++ app/res/tpl/model/country/edit.php
@@ -57,4 +57,45 @@
             required="required" />
     </div>
 </fieldset>
+<div class="tab-container">
+    <?php Flight::render('shared/navigation/tabs', array(
+        'tab_id' => 'country-tabs',
+        'tabs' => array(
+            'country-translation' => I18n::__('country_translation_tab')
+        ),
+        'default_tab' => 'country-translation'
+    )) ?>
+    <fieldset
+        id="country-translation"
+        class="tab">
+        <legend class="verbose"><?php echo I18n::__('tokeni18n_legend') ?></legend>
+        <?php foreach (R::findAll('language') as $_id => $_language): ?>
+            <?php $_tokeni18n = $record->i18n($_language->iso) ?>
+            <div class="row <?php echo ($_tokeni18n->hasError('name')) ? 'error' : ''; ?>">
+                <input
+                    type="hidden"
+                    name="dialog[ownCountryi18n][<?php echo $_id ?>][type]"
+                    value="countryi18n" />
+                <input
+                    type="hidden"
+                    name="dialog[ownCountryi18n][<?php echo $_id ?>][id]"
+                    value="<?php echo $_tokeni18n->getId() ?>" />
+                <input
+                    type="hidden"
+                    name="dialog[ownCountryi18n][<?php echo $_id ?>][language]"
+                    value="<?php echo $_tokeni18n->language ?>" />
+                <label
+                    for="translation-<?php echo $_language->iso ?>-<?php echo $_tokeni18n->getId() ?>">
+                    <?php echo I18n::__('language_'.$_tokeni18n->language) ?>
+                </label>
+                <textarea
+                    id="translation-<?php echo $_language->iso ?>-<?php echo $_tokeni18n->getId() ?>"
+                    class="scaleable"
+                    name="dialog[ownCountryi18n][<?php echo $_id ?>][name]"
+                    cols="60"
+                    rows="2"><?php echo htmlspecialchars($_tokeni18n->name) ?></textarea>
+            </div>
+        <?php endforeach ?>
+    </fieldset>
+</div>
 <!-- end of country edit form -->
