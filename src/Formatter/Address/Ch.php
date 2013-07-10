1db807861b2ed4a7cc157e99ad6e116169162b67
Stephan <info@sah-company.com>
2013-07-10 09:36:28 +0200

    Added address formatters for CH, FI, NL and LU
    See www.upu.int

---
+++ src/Formatter/Address/Ch.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Cinnebar.
+ *
+ * My lightweight no-framework framework written in PHP.
+ *
+ * @package Cinnebar
+ * @author $Author$
+ * @version $Id$
+ */
+
+/**
+ * Formats a switzerland postal address.
+ *
+ * See {@link http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/cheEn.pdf} on how
+ * a german postal address should be formatted.
+ *
+ * @package Cinnebar
+ * @subpackage Formatter
+ * @version $Id$
+ */
+class Formatter_Address_Ch extends Formatter
+{
+    /**
+     * Formats attributes of a bean.
+     *
+     * @param RedBean_OODBBean $bean to format
+     * @return string $formattedString
+     */
+    public function format(RedBean_OODBBean $bean)
+    {
+        return sprintf("%s\n%s %s\n%s", $bean->street, $bean->zip, mb_strtoupper($bean->city), 'SWITZERLAND');
+    }
+}
---
+++ src/Formatter/Address/Lu.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Cinnebar.
+ *
+ * My lightweight no-framework framework written in PHP.
+ *
+ * @package Cinnebar
+ * @author $Author$
+ * @version $Id$
+ */
+
+/**
+ * Formats a luxembourg postal address.
+ *
+ * See {@link http://www.upu.int/fileadmin/documentsFiles/activities/addressingUnit/luxEn.pdf} on how
+ * a german postal address should be formatted.
+ *
+ * @package Cinnebar
+ * @subpackage Formatter
+ * @version $Id$
+ */
+class Formatter_Address_Lu extends Formatter
+{
+    /**
+     * Formats attributes of a bean.
+     *
+     * @param RedBean_OODBBean $bean to format
+     * @return string $formattedString
+     */
+    public function format(RedBean_OODBBean $bean)
+    {
+        return sprintf("%s\n%s %s\n%s", $bean->street, 'L-'.$bean->zip, $bean->city, 'LUXEMBOURG');
+    }
+}
