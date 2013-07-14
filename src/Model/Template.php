b05ec69464849f327c4d6902ecd8755e3b0ab383
Stephan <info@sah-company.com>
2013-07-14 22:40:12 +0200

    Model_Template writes and deletes htm*, txt* files
    For the html and txt attributes a file is written to app/tpl/cache when
    they are not empty

--- src/Model/Template.php
+++ src/Model/Template.php
@@ -45,4 +45,36 @@ class Model_Template extends Model
     {
         $this->addValidator('name', new Validator_HasValue());
     }
+    
+    /**
+     * After update write frontend and backend tpl as file if neccessary.
+     */
+    public function after_update()
+    {
+        if ($this->bean->html) {
+            $filename = Flight::get('flight.views.path').'/cache/htm'.md5($this->bean->name).'.php';
+            file_put_contents($filename, $this->bean->html);
+        }
+        if ($this->bean->txt) {
+            $filename = Flight::get('flight.views.path').'/cache/txt'.md5($this->bean->name).'.php';
+            file_put_contents($filename, $this->bean->txt);
+        }
+    }
+    
+    /**
+     * After delete clean up the cached frontend and backend template files.
+     */
+    public function after_delete()
+    {
+        if (file_exists($filename = Flight::get('flight.views.path') . 
+                                    '/cache/htm' . md5($this->bean->name) . '.php')) {
+            unlink($filename = Flight::get('flight.views.path') . 
+                                '/cache/htm' . md5($this->bean->name) . '.php');
+        }
+        if (file_exists($filename = Flight::get('flight.views.path') . 
+                                    '/cache/txt' . md5($this->bean->name) . '.php')) {
+            unlink($filename = Flight::get('flight.views.path') . 
+                                    '/cache/txt' . md5($this->bean->name) . '.php');
+        }
+    }
 }
