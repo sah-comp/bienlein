fcd961e05a12f08df7bede467e09140c69a773d8
Stephan <info@sah-company.com>
2014-09-02 22:33:30 +0200

    Gestalt now uses attach and detach for limb sub beans

--- app/res/tpl/model/gestalt/edit.php
+++ app/res/tpl/model/gestalt/edit.php
@@ -73,11 +73,14 @@
 
         <div class="row">
             
-            
+
+            <div class="span3">
+                &nbsp;
+            </div>
             <div class="span1">
                 <?php echo I18n::__('limb_label_active') ?>
             </div>
-            <div class="span2">
+            <div class="span1">
                 <?php echo I18n::__('limb_label_sequence') ?>
             </div>
             <div class="span2">
@@ -94,105 +97,23 @@
             </div>
 
         </div>
+        
+        <div
+            id="person-<?php echo $record->getId() ?>-address-container"
+            class="container attachable detachable sortable">
 
         <?php $_limbs = $record->with(' ORDER BY sequence ASC ')->ownLimb ?>
-        <?php $_limbs[] = R::dispense('limb') ?>
+        <?php if (count($_limbs) == 0) $_limbs[] = R::dispense('limb') ?>
         <?php $index = 0 ?>
         <?php foreach ($_limbs as $_limb_id => $_limb): ?>
         <?php $index++ ?>
-        
-        <div class="row">
-            
-            <div>                
-                <input
-                    type="hidden"
-                    name="dialog[ownLimb][<?php echo $index ?>][type]"
-                    value="<?php echo $_limb->getMeta('type') ?>" />
-                <input
-                    type="hidden"
-                    name="dialog[ownLimb][<?php echo $index ?>][id]"
-                    value="<?php echo $_limb->getId() ?>" />
-            </div>
-            
-            <div class="span1">
-                <input
-                    type="hidden"
-                    name="dialog[ownLimb][<?php echo $index ?>][active]"
-                    value="0" />
-                <input
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-active"
-                    type="checkbox"
-                    name="dialog[ownLimb][<?php echo $index ?>][active]"
-                    <?php echo ($_limb->active) ? 'checked="checked"' : '' ?>
-                    value="1" />
-            </div>
-            <div class="span2">
-                <input
-                    type="number"
-                    min="0"
-                    step="10"
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-sequence"
-                    name="dialog[ownLimb][<?php echo $index ?>][sequence]"
-                    value="<?php echo htmlspecialchars($_limb->sequence) ?>" />
-            </div>
-            <div class="span2">
-                <input
-                    type="text"
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-name"
-                    name="dialog[ownLimb][<?php echo $index ?>][name]"
-                    value="<?php echo htmlspecialchars($_limb->name) ?>" />
-            </div>
-            <div class="span2">
-                <select
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-kind"
-                    name="dialog[ownLimb][<?php echo $index ?>][kind]">
-                    <option
-                        value="">
-                        <?php echo I18n::__('select_one_or_leave_empty') ?>
-                    </option>
-                    <?php foreach ($_limb->getKinds() as $_attr_name): ?>
-                    <option
-                        value="<?php echo $_attr_name ?>"
-                        <?php echo ($_limb->kind == $_attr_name) ? 'selected="selected"' : '' ?>>
-                        <?php echo I18n::__(sprintf('limb_kind_%s', $_attr_name)) ?>
-                    </option>
-                    <?php endforeach ?>
-                </select>
-            </div>
-            <div class="span2">
-                <select
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-tag"
-                    name="dialog[ownLimb][<?php echo $index ?>][tag]">
-                    <option
-                        value="">
-                        <?php echo I18n::__('select_one_or_leave_empty') ?>
-                    </option>
-                    <?php foreach ($_limb->getTags() as $_attr_name): ?>
-                    <option
-                        value="<?php echo $_attr_name ?>"
-                        <?php echo ($_limb->tag == $_attr_name) ? 'selected="selected"' : '' ?>>
-                        <?php echo I18n::__(sprintf('limb_tag_%s', $_attr_name)) ?>
-                    </option>
-                    <?php endforeach ?>
-                </select>
-            </div>
-            <div class="span1">
-                <input
-                    type="hidden"
-                    name="dialog[ownLimb][<?php echo $index ?>][filter]"
-                    value="0" />
-                <input
-                    id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-filter"
-                    type="checkbox"
-                    name="dialog[ownLimb][<?php echo $index ?>][filter]"
-                    <?php echo ($_limb->filter) ? 'checked="checked"' : '' ?>
-                    value="1" />
-            </div>
-
-        </div>
-        
-        
+        <?php Flight::render('model/gestalt/own/limb', array(
+            'record' => $record,
+            '_limb' => $_limb,
+            'index' => $index
+        )) ?>
         <?php endforeach ?>
+        </div>
     </fieldset>
 </div>
 <!-- end of gestalt edit form -->
---
+++ app/res/tpl/model/gestalt/own/limb.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Cinnebar.
+ *
+ * @package Cinnebar
+ * @subpackage Template
+ * @author $Author$
+ * @version $Id$
+ */
+?>
+<!-- limb edit subform -->
+<fieldset
+    id="gestalt-<?php echo $record->getId() ?>-ownlimb-<?php echo $_limb->getId() ?>">
+    <legend class="verbose"><?php echo I18n::__('gestalt_legend_limb') ?></legend>
+    <a
+    	href="<?php echo Url::build(sprintf('/admin/gestalt/detach/address/%d', $_limb->getId())) ?>"
+    	class="ir detach"
+    	title="<?php echo I18n::__('scaffold_detach') ?>"
+    	data-target="person-<?php echo $record->getId() ?>-ownaddress-<?php echo $_limb->getId() ?>">
+    		<?php echo I18n::__('scaffold_detach') ?>
+    </a>
+    <a
+    	href="<?php echo Url::build(sprintf('/admin/gestalt/attach/own/limb/%d', $record->getId())) ?>"
+    	class="ir attach"
+    	title="<?php echo I18n::__('scaffold_attach') ?>"
+    	data-target="person-<?php echo $record->getId() ?>-address-container">
+    		<?php echo I18n::__('scaffold_attach') ?>
+    </a>
+
+    <div>                
+        <input
+            type="hidden"
+            name="dialog[ownLimb][<?php echo $index ?>][type]"
+            value="<?php echo $_limb->getMeta('type') ?>" />
+        <input
+            type="hidden"
+            name="dialog[ownLimb][<?php echo $index ?>][id]"
+            value="<?php echo $_limb->getId() ?>" />
+    </div>
+
+    <div class="row">
+    
+        <div class="span3">
+            &nbsp;
+        </div>
+        <div class="span1">
+            <input
+                type="hidden"
+                name="dialog[ownLimb][<?php echo $index ?>][active]"
+                value="0" />
+            <input
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-active"
+                type="checkbox"
+                name="dialog[ownLimb][<?php echo $index ?>][active]"
+                <?php echo ($_limb->active) ? 'checked="checked"' : '' ?>
+                value="1" />
+        </div>
+        <div class="span1">
+            <input
+                type="number"
+                min="0"
+                step="10"
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-sequence"
+                name="dialog[ownLimb][<?php echo $index ?>][sequence]"
+                value="<?php echo htmlspecialchars($_limb->sequence) ?>" />
+        </div>
+        <div class="span2">
+            <input
+                type="text"
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-name"
+                name="dialog[ownLimb][<?php echo $index ?>][name]"
+                value="<?php echo htmlspecialchars($_limb->name) ?>" />
+        </div>
+        <div class="span2">
+            <select
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-kind"
+                name="dialog[ownLimb][<?php echo $index ?>][kind]">
+                <option
+                    value="">
+                    <?php echo I18n::__('select_one_or_leave_empty') ?>
+                </option>
+                <?php foreach ($_limb->getKinds() as $_attr_name): ?>
+                <option
+                    value="<?php echo $_attr_name ?>"
+                    <?php echo ($_limb->kind == $_attr_name) ? 'selected="selected"' : '' ?>>
+                    <?php echo I18n::__(sprintf('limb_kind_%s', $_attr_name)) ?>
+                </option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="span2">
+            <select
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-tag"
+                name="dialog[ownLimb][<?php echo $index ?>][tag]">
+                <option
+                    value="">
+                    <?php echo I18n::__('select_one_or_leave_empty') ?>
+                </option>
+                <?php foreach ($_limb->getTags() as $_attr_name): ?>
+                <option
+                    value="<?php echo $_attr_name ?>"
+                    <?php echo ($_limb->tag == $_attr_name) ? 'selected="selected"' : '' ?>>
+                    <?php echo I18n::__(sprintf('limb_tag_%s', $_attr_name)) ?>
+                </option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="span1">
+            <input
+                type="hidden"
+                name="dialog[ownLimb][<?php echo $index ?>][filter]"
+                value="0" />
+            <input
+                id="gestalt-<?php echo $record->getId() ?>-limb-<?php echo $_limb->getId() ?>-filter"
+                type="checkbox"
+                name="dialog[ownLimb][<?php echo $index ?>][filter]"
+                <?php echo ($_limb->filter) ? 'checked="checked"' : '' ?>
+                value="1" />
+        </div>
+
+    </div>
+</fieldset>
+<!-- /limb edit subform -->
