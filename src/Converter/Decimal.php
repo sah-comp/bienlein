b7c4d07f195086098c4478451de346e0112bf0a0
Stephan <info@sah-company.com>
2014-12-19 17:40:52 +0100

    Fixed a problem with converting to float value
    http://php.net/manual/de/function.floatval.php#114486

--- src/Converter/Decimal.php
+++ src/Converter/Decimal.php
@@ -26,6 +26,19 @@ class Converter_Decimal extends Converter
      */
     public function convert($value)
     {
-        return (float)str_replace(',', '.', $value);
+        //return (float)str_replace(',', '.', $value);
+        $dotPos = strrpos($num, '.');
+        $commaPos = strrpos($num, ',');
+        $sep = (($dotPos > $commaPos) && $dotPos) ? $dotPos : 
+            ((($commaPos > $dotPos) && $commaPos) ? $commaPos : false);
+
+        if (!$sep) {
+            return floatval(preg_replace("/[^0-9]/", "", $num));
+        } 
+
+        return floatval(
+            preg_replace("/[^0-9]/", "", substr($num, 0, $sep)) . '.' .
+            preg_replace("/[^0-9]/", "", substr($num, $sep+1, strlen($num)))
+        );
     }
 }
