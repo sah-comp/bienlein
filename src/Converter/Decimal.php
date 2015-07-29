0f427bef5713706dd3f39d7f56f3f2be1f86fa9e
Stephan <info@sah-company.com>
2015-07-30 00:14:13 +0200

    Fixed wrong parameter name

--- src/Converter/Decimal.php
+++ src/Converter/Decimal.php
@@ -21,12 +21,11 @@ class Converter_Decimal extends Converter
     /**
      * Replaces comma against a decimal point and casts the value as float.
      *
-     * @param mixed $value
+     * @param mixed $num
      * @return float $floatingPointValue
      */
-    public function convert($value)
+    public function convert($num)
     {
-        //return (float)str_replace(',', '.', $value);
         $dotPos = strrpos($num, '.');
         $commaPos = strrpos($num, ',');
         $sep = (($dotPos > $commaPos) && $dotPos) ? $dotPos : 
