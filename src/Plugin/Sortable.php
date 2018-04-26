acae5a46426e791e4fd05fcef99ff8a8ce7a44a9
Stephan <info@sah-company.com>
2018-04-26 19:55:31 +0200

    Beautified

--- src/Plugin/Sortable.php
+++ src/Plugin/Sortable.php
@@ -18,24 +18,26 @@
  */
 class Plugin_Sortable extends Plugin
 {
-	/**
-	 * Updates all given beans of a certain type after being sorted by client in frontend.
-	 *
-	 * @param string $type
-	 * @param string $varname is the name of the variable the list of sortable items is in
-	 * @return void
-	 */
-	public function execute($type, $varname = 'sequence')
-	{
+    /**
+     * Updates all given beans of a certain type after being sorted by client in frontend.
+     *
+     * @param string $type
+     * @param string $varname is the name of the variable the list of sortable items is in
+     * @return void
+     */
+    public function execute($type, $varname = 'sequence')
+    {
         session_start();
         if ($sequence = Flight::request()->query->$varname) {
             foreach ($sequence as $n => $id) {
                 $bean = R::load($type, $id);
-                if ( ! $bean->getId()) continue; // skip if that id does not exist
+                if (! $bean->getId()) {
+                    continue;
+                } // skip if that id does not exist
                 $bean->sequence = $n;
                 R::store($bean);
             }
         }
-		return true;
-	}
+        return true;
+    }
 }
