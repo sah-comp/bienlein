f61b2cef9dba78f1f6c57c414bb42d86690e653d
Stephan <info@sah-company.com>
2018-10-01 15:29:26 +0200

    Splitter config.php into bootstrap.php and config.php
    Now config.php holds definition of constants which are used in
    bootstrap.php to setup the system.

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -11,17 +11,17 @@
 /**
  * Set internal encoding to UTF-8.
  */
-mb_internal_encoding('UTF-8');
+mb_internal_encoding(CINNEBAR_INTERNAL_ENCODING);
 
 /**
  * Set the time zone.
  */
-date_default_timezone_set('Europe/Berlin');
+date_default_timezone_set(CINNEBAR_DATE_TIMEZONE);
 
 /**
  * Define constant install password.
  */
-define('CINNEBAR_INSTALL_PASS', password_hash('Secret', PASSWORD_DEFAULT));
+define('CINNEBAR_INSTALL_PASS', password_hash(CINNEBAR_INSTALL_PASSWORD, PASSWORD_DEFAULT));
 
 /**
  * Error logging on.
@@ -34,16 +34,13 @@ Flight::set('flight.log_errors', true);
 Flight::path(__DIR__ . '/../../src');
 Flight::path(__DIR__ . '/../../app');
 
-/**
- * Define constant with path to the vendor directory.
- */
 define('VENDORS', __DIR__.'/../../vendor/');
 
 /**
  * Setup our database.
  */
-R::setup('mysql:host=localhost;dbname=DBNAME', 'UNAME', 'SECRET');
-R::freeze(false);
+R::setup('mysql:host=' . CINNEBAR_DB_HOST . ';dbname=' . CINNEBAR_DB_NAME, CINNEBAR_DB_USER, CINNEBAR_DB_PASSWORD);
+R::freeze(true);
 //R::$writer->setUseCache(true);
 
 /**
@@ -75,7 +72,7 @@ Flight::set('media_path', '/upload');
 /**
  * Set the maximum file size for uploads in bytes.
  */
-Flight::set('max_upload_size', 2097152);
+Flight::set('max_upload_size', CINNEBAR_MAX_UPLOAD_SIZE);
 
 /**
  * Set the directory where to store user uploaded files.
@@ -85,7 +82,7 @@ Flight::set('upload_dir', __DIR__ . '/../../public/upload');
 /**
  * Set the default language.
  */
-Flight::set('default_language', 'de');
+Flight::set('default_language', CINNEBAR_DEFAULT_LANGUAGE);
 
 /**
  * Set possible languages.
@@ -97,58 +94,62 @@ Flight::set('possible_languages', R::dispense('language')->getEnabled(Flight::ge
  *
  * This get changed by our routes if the called url begins with a 2-character iso code.
  */
-Flight::set('language', 'de');
+Flight::set('language', CINNEBAR_DEFAULT_LANGUAGE);
 
 /**
  * Sets some template for localization.
  */
 Flight::set('templates', array(
-    'date' => '%x',
-    'time' => '%X',
-    'datetime' => '%x %X'
+    'date' => CINNEBAR_TEMPLATE_DATE,
+    'time' => CINNEBAR_TEMPLATE_TIME,
+    'datetime' => CINNEBAR_TEMPLATE_DATETIME
 ));
 
 /**
  * Setting.
+ *
+ * Load the setting bean. There is ususally only one which ID is 1.
  */
-Flight::map('setting', function() {
-    return R::load('setting', 1);//setting there can only be one
+Flight::map('setting', function () {
+    return R::load('setting', CINNEBAR_SETTING_BEAN_ID);
 });
 
 /**
  * Textile.
  */
-Flight::map('textile', function($text, $restricted = false) {
-    $parser = new Textile('html5');
-    return $parser->TextileThis($text);
+Flight::map('textile', function ($text, $restricted = false, $mode = 'html5') {
+    $parser = new \Netcarver\Textile\Parser($mode);
+    return $parser->setRestricted($restricted)->parse($text);
 });
 
 /**
  * Blessed folder.
+ *
+ * The blessed folder is the system folder (domain bean).
  */
-Flight::map('blessedfolder', function() {
-    return R::load('domain', Flight::setting()->blessedfolder);//
+Flight::map('blessedfolder', function () {
+    return R::load('domain', Flight::setting()->blessedfolder);
 });
 
 /**
  * Sites folder.
  */
-Flight::map('sitesfolder', function() {
-    return R::load('domain', Flight::setting()->sitesfolder);//
+Flight::map('sitesfolder', function () {
+    return R::load('domain', Flight::setting()->sitesfolder);
 });
 
 /**
- * Blessed folder.
+ * Base currency.
  */
-Flight::map('basecurrency', function() {
+Flight::map('basecurrency', function () {
     return R::load('currency', Flight::setting()->basecurrency);
 });
 
 /**
  * Sets a locale category according to the current language.
  */
-Flight::map('setlocale', function($category = LC_TIME) {
-    return setlocale($category, Flight::get('language').'_'.strtoupper(Flight::get('language')).'.UTF-8');
+Flight::map('setlocale', function ($category = LC_TIME) {
+    return setlocale($category, Flight::get('language').'_'.strtoupper(Flight::get('language')).'.' . CINNEBAR_INTERNAL_ENCODING);
 });
 
 // There shall be non url rewriter and session id gets handled by cookies only
@@ -160,15 +161,17 @@ ini_set('session.use_only_cookies', '1');
 /**
  * Define the maximum session lifetime in seconds.
  */
-define('MAX_SESSION_LIFETIME', 14400); // 4 hours
+define('MAX_SESSION_LIFETIME', CINNEBAR_MAX_SESSION_LIFETIME); // 4 hours
 
 $sessionhandler = new Sessionhandler_Database();
-session_set_save_handler(array($sessionhandler, 'open'),
+session_set_save_handler(
+    array($sessionhandler, 'open'),
                          array($sessionhandler, 'close'),
                          array($sessionhandler, 'read'),
                          array($sessionhandler, 'write'),
                          array($sessionhandler, 'destroy'),
-                         array($sessionhandler, 'gc'));
+                         array($sessionhandler, 'gc')
+);
 register_shutdown_function('session_write_close');
 
 /**
@@ -177,4 +180,4 @@ register_shutdown_function('session_write_close');
  * If you have changed session parameter or handling in a new release change
  * the session name to ensure that older sessions are no longer used.
  */
-session_name('CINNEBARv1');
+session_name(CINNEBAR_SESSION_COOKIE_NAME);
