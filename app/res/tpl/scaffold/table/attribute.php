d7cf0e9611545d80b543348839316caf84a2e808
sah-comp <info@sah-company.com>
2020-08-15 23:18:54 +0200

    Dropped title tag

--- app/res/tpl/scaffold/table/attribute.php
+++ app/res/tpl/scaffold/table/attribute.php
@@ -11,7 +11,6 @@
 <a
     href="<?php echo Url::build(sprintf('/admin/%s/inline/%s/%d', $_record->getMeta('type'), $_attribute['name'], $_record->getId())) ?>"
     class="fox-on-the-run"
-    title="Inline Editor"
     data-target="<?php echo $_target ?>">
     <?php if (isset($_attribute['callback'])): ?>
         <?php echo htmlspecialchars($_record->{$_attribute['callback']['name']}($_attribute['name'])) ?>
