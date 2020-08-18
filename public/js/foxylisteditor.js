6bd0ed1bc8980dfe9b82a68157c260044cc87e32
sah-comp <info@sah-company.com>
2020-08-18 19:48:44 +0200

    Added foxy list editor javascript
    When a user account is using foxy list editor this js will be loaded, making use of table-edit.js.

--- public/js/foxylisteditor.js
+++ public/js/foxylisteditor.js
@@ -6,10 +6,33 @@ $(document).ready(function() {
      * @see https://github.com/nathancahill/table-edits
      */
     $('table.scaffold tr').editable({
+        buttonSelector: '.action-inline-edit',
+        dropdowns: {
+            'enabled': [{
+                    val: 0,
+                    text: 'Nein'
+                },
+                {
+                    val: 1,
+                    text: 'Ja'
+                }
+            ]
+        },
         save: function(values) {
             var type = $(this).data('type');
             var id = $(this).data('id');
-            $.post('/api/update/' + type + '/' + id, values);
+            $.post('/api/update/' + type + '/' + id, values, function(data) {
+                console.log(data.result);
+                if (data.result == 'bad') {
+                    //what shall we do with the drunken sailor?
+                }
+            }, 'json');
+        },
+        edit: function(values) {
+            $(this).find("input[type=text]").filter(":first").focus();
+        },
+        cancel: function(values) {
+            //user cancel editing by pressing ESC key.
         }
     });
 });
