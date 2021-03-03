a6d1842cac2656cbe10942bd935e8b762a215a90
sah-comp <info@sah-company.com>
2021-03-03 19:17:37 +0100

    PHP 8 Compatibility

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ require __DIR__ . '/../vendor/autoload.php';
 /**
  * RedbeanPHP Version 5.5.
  */
-require __DIR__ . '/../lib/redbean/rb-5.5.php';
+require __DIR__ . '/../lib/redbean/rb-mysql.php';
 require __DIR__ . '/../lib/redbean/Plugin/Cooker.php';
 
 /**
