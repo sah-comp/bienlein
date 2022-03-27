8097a316d38e74006c5c5762423a622aa9de261e
sah-comp <info@sah-company.com>
2022-03-27 10:10:53 +0200

    Dropped phpunit after security warning

--- src/Controller/Cms.php
+++ src/Controller/Cms.php
@@ -20,10 +20,10 @@ class Controller_Cms extends Controller
     /**
      * Holds a flag to decide wether to trigger a click-simulation on meta tab or not.
      *
-     * @var bool 
+     * @var bool
      */
     public $trigger_meta = false;
-    
+
     /**
      * Renders a domain when it has pages otherwise shows a 404 error.
      *
@@ -33,10 +33,12 @@ class Controller_Cms extends Controller
     public function frontend(RedBeanPHP\OODBBean $domain)
     {
         $template_data = $domain->getContent(Flight::get('language'));
-		Flight::response()->cache(false);
-        if ( ! Flight::request()->query->preview) Flight::lastModified($domain->lastmodified);
+        Flight::response()->cache(false);
+        if (! Flight::request()->query->preview) {
+            Flight::lastModified((int)$domain->lastmodified);
+        }
         $tpl = $template_data['mytemplate']->name;
-        if ( ! Flight::view()->exists($tpl)) {
+        if (! Flight::view()->exists($tpl)) {
             $tpl = 'cache/htm'.md5($template_data['mytemplate']->name);
         }
         Flight::render($tpl, $template_data);
@@ -55,26 +57,26 @@ class Controller_Cms extends Controller
         session_start();
         Auth::check();
         Permission::check(Flight::get('user'), 'cms', 'index');
-		// Pick up the pieces
-		Flight::render('shared/notification', array(), 'notification');
+        // Pick up the pieces
+        Flight::render('shared/notification', array(), 'notification');
         Flight::render('shared/navigation/account', array(), 'navigation_account');
         Flight::render('shared/navigation/main', array(), 'navigation_main');
         Flight::render('shared/navigation', array(), 'navigation');
-		Flight::render('shared/header', array(), 'header');
-		Flight::render('shared/footer', array(), 'footer');
+        Flight::render('shared/header', array(), 'header');
+        Flight::render('shared/footer', array(), 'footer');
         Flight::render('cms/toolbar', array(), 'toolbar');
         Flight::render('cms/index', array(
             'sitemap' => Flight::sitesfolder()
                                 ->hierMenu('/cms/node/', Flight::get('user')->getLanguage(), true, 'id')
                                 ->render(array('class' => 'sitemap-navigation clearfix'))
         ), 'content');
-		// Use a layout to pack it all
+        // Use a layout to pack it all
         Flight::render('html5', array(
             'title' => I18n::__('admin_head_title'),
             'language' => Flight::get('language')
         ));
     }
-    
+
     /**
      * Edit a node.
      *
@@ -88,40 +90,38 @@ class Controller_Cms extends Controller
         $domain = R::load('domain', $id);
         //$root = $domain->getRoot($stop_at = Flight::sitesfolder()->getId());
         //Permission::check(Flight::get('user'), $domain, 'edit');
-		// Pick up the pieces
-		//echo 'You want to edit node '.$id;
-		$pages = $domain->getPages(Flight::get('user')->getLanguage());
-		if (empty($pages)) {
-		    $page = R::dispense('page');
-		}
-		else {
-		    if ($page_id && isset($pages[$page_id])) {
-		        $page = $pages[$page_id];
-		    }
-		    else {
-		        $page = reset($pages);
-		    }
-		}
-		Flight::render('cms/container/addpage', array(
-		    'domain' => $domain
-		), 'form_addpage');
-		Flight::render('cms/container/pages', array(
-		    'page' => $page,
-		    'pages' => $pages
-		), 'pages');
-		Flight::render('cms/container/meta', array(
-		    'page' => $page
-		), 'page_meta');
-		Flight::render('cms/container/preview', array(
-		    'domain' => $domain
-		), 'domain_preview');
-		Flight::render('cms/container/page', array(
-		    'trigger_meta' => $this->trigger_meta,
-		    'page' => $page
-		), 'page');
-		Flight::render('cms/container/content', array());
+        // Pick up the pieces
+        //echo 'You want to edit node '.$id;
+        $pages = $domain->getPages(Flight::get('user')->getLanguage());
+        if (empty($pages)) {
+            $page = R::dispense('page');
+        } else {
+            if ($page_id && isset($pages[$page_id])) {
+                $page = $pages[$page_id];
+            } else {
+                $page = reset($pages);
+            }
+        }
+        Flight::render('cms/container/addpage', array(
+            'domain' => $domain
+        ), 'form_addpage');
+        Flight::render('cms/container/pages', array(
+            'page' => $page,
+            'pages' => $pages
+        ), 'pages');
+        Flight::render('cms/container/meta', array(
+            'page' => $page
+        ), 'page_meta');
+        Flight::render('cms/container/preview', array(
+            'domain' => $domain
+        ), 'domain_preview');
+        Flight::render('cms/container/page', array(
+            'trigger_meta' => $this->trigger_meta,
+            'page' => $page
+        ), 'page');
+        Flight::render('cms/container/content', array());
     }
-    
+
     /**
      * Edit a page.
      *
@@ -132,18 +132,18 @@ class Controller_Cms extends Controller
         session_start();
         Auth::check();
         $page = R::load('page', $id);
-		Flight::render('cms/container/meta', array(
-		    'page' => $page
-		), 'page_meta');
-		Flight::render('cms/container/preview', array(
-		    'domain' => $page->domain
-		), 'domain_preview');
-		Flight::render('cms/container/page', array(
-		    'trigger_meta' => $this->trigger_meta,
-		    'page' => $page
-		));
+        Flight::render('cms/container/meta', array(
+            'page' => $page
+        ), 'page_meta');
+        Flight::render('cms/container/preview', array(
+            'domain' => $page->domain
+        ), 'domain_preview');
+        Flight::render('cms/container/page', array(
+            'trigger_meta' => $this->trigger_meta,
+            'page' => $page
+        ));
     }
-    
+
     /**
      * Update a page meta information on a POST request.
      *
@@ -154,21 +154,19 @@ class Controller_Cms extends Controller
         session_start();
         Auth::check();
         try {
-            $page = R::graph( Flight::request()->data->dialog, TRUE );
+            $page = R::graph(Flight::request()->data->dialog, true);
             if (Flight::request()->data->delete) {
                 R::trash($page);
                 return $this->node($page->domain_id);
-            }
-            else {
+            } else {
                 R::store($page);
                 return $this->node($page->domain_id, $page->getId());
             }
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             error_log($e);
         }
     }
-    
+
     /**
      * Edit a slice.
      *
@@ -180,25 +178,23 @@ class Controller_Cms extends Controller
         Auth::check();
         if (Flight::request()->method == 'POST') {
             try {
-                $slice = R::graph( Flight::request()->data->dialog, TRUE );
+                $slice = R::graph(Flight::request()->data->dialog, true);
                 if (Flight::request()->data->delete) {
                     R::trash($slice);
                     echo '';
                     return;
-                }
-                else {
+                } else {
                     R::store($slice);
                     echo $slice->renderFrontend();
                     return;
                 }
-            }
-            catch (Exception $e) {
+            } catch (Exception $e) {
                 error_log($e);
             }
         }
         $slice = R::load('slice', $id)->renderBackend();
     }
-    
+
     /**
      * Add a new item on a POST request.
      *
@@ -217,29 +213,27 @@ class Controller_Cms extends Controller
     protected function add_page()
     {
         try {
-            $page = R::graph( Flight::request()->data->dialog, TRUE );
+            $page = R::graph(Flight::request()->data->dialog, true);
             R::store($page);
             $this->trigger_meta = true;
             return $this->node($page->domain_id, $page->getId());
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             error_log($e);
         }
     }
-    
+
     /**
      * Add a new slice.
      */
     protected function add_slice()
     {
         try {
-            $slice = R::graph( Flight::request()->data->dialog, TRUE );
+            $slice = R::graph(Flight::request()->data->dialog, true);
             $slice->sequence = $slice->page->withCondition('region_id = ?', array($slice->region_id))->countOwn('slice');
             R::store($slice);
-    		$slice->renderBackend('form');
-    		Flight::render('cms/container/slice', array());
-        }
-        catch (Exception $e) {
+            $slice->renderBackend('form');
+            Flight::render('cms/container/slice', array());
+        } catch (Exception $e) {
             error_log($e);
         }
     }
